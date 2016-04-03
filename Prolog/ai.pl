5fa44aa38eb53afeb5c8097df78b2d59fba43376
jmaire <julien.bpx@gmail.com>
2016-04-03 19:14:21 +0200

    amélioration avec des select

--- Prolog/ai.pl
+++ Prolog/ai.pl
@@ -10,12 +10,16 @@ range(Low,High,Sol):-
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-vide('_'):-!.
-nul(0):-!.
+taille(9).
+vide('_').
+nonvide(NV):-
+	vide(V),
+	NV\=V.
+nul(0).
 joueur(1).
 joueur(2).
-joueurSuivant(1,2):-!.
-joueurSuivant(2,1):-!.
+joueurSuivant(1,2).
+joueurSuivant(2,1).
 
 morpionVide([V,V,V,V,V,V,V,V,V]):-
 	vide(V).
@@ -32,47 +36,54 @@ morpionGagne([_,_,A,_,_,A,_,_,A],A).
 morpionGagne([A,_,_,_,A,_,_,_,A],A).
 morpionGagne([_,_,A,_,A,_,A,_,_],A).
 
+listeCasesJouables([0,1,2,3,4,5,6,7,8]).
+
+genererListeCasesJouables(Pm,Lm):-
+	genererListeCasesJouables(0,Pm,Lm).
+genererListeCasesJouables(_,[],[]):-!.
+genererListeCasesJouables(N,[V|Pm],[N|Lm]):-
+	vide(V),!,
+	N1 is N+1,
+	genererListeCasesJouables(N1,Pm,Lm).
+genererListeCasesJouables(N,[NV|Pm],Lm):-
+	nonvide(NV),!,
+	N1 is N+1,
+	genererListeCasesJouables(N1,Pm,Lm).
+
+selectionnerCaseJouable(Morp,ICase):-
+	genererListeCasesJouables(Morp,Lm),
+	select(ICase,Lm,_).
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % Le joueur J joue dans la case I (si libre) du morpion Morp, le résultat est stocké en Morpf
-jouerALaCaseI(Morp,J,ICase,Morpf):-
-	vide(V),
+jouerALaCase(Morp,J,ICase,Morpf):-
+	vide(V), % TOREMOVE
 	length(BeforeI,ICase),
 	append(BeforeI,[V|PastI],Morp),
 	append(BeforeI,[J|PastI],Morpf).
-
-% Le joueur J joue dans une case du morpion Morp, le résultat est stocké en Morpf
-jouerDansLeMorpion(Morp,J,ICase,Morpf):-
-	range(0,8,ICase),
-	jouerALaCaseI(Morp,J,ICase,Morpf).
 
 %
 jouer(IMorp,Pm,Pl,J,ICase,Pmf,Plf):-
 	length(BeforeIl,IMorp),
 	append(BeforeIl,[Morp|PastIl],Pl),
-	jouerDansLeMorpion(Morp,J,ICase,Morpf),
+	selectionnerCaseJouable(Morp,ICase),
+	jouerALaCase(Morp,J,ICase,Morpf),
 	verifierMorpionGagnant(Pm,Morpf,IMorp,Pmf),
 	append(BeforeIl,[Morpf|PastIl],Plf).
 
-% Debut
-jouerIci(IMorp0,Pm,Pl,J,ICase,Pmf,Plf):-
-	IMorp0 is -1,!,
-	vide(V),
-	range(0,8,IMorp),
-	length(BeforeIm,IMorp),
-	append(BeforeIm,[V|_],Pm), % morpion non termine
-	jouer(IMorp,Pm,Pl,J,ICase,Pmf,Plf).
-jouerIci(IMorp0,Pm,Pl,J,ICase,Pmf,Plf):-
-	vide(V),
+trouverMorpionJouable(_,IMorp0,IMorp):-
+	IMorp0 is -1,!, % premier coup des croix
+	listeCasesJouables(Lm),
+	select(IMorp,Lm,_).
+trouverMorpionJouable(Pm,IMorp0,IMorp):-
+	nonvide(NV),
 	length(BeforeIm0,IMorp0),
-	append(BeforeIm0,[NV|_],Pm),
-	NV \= V,!,
-	range(0,8,IMorp), IMorp\=IMorp0,
-	length(BeforeIm,IMorp),
-	append(BeforeIm,[V|_],Pm), % morpion non termine
-	jouer(IMorp,Pm,Pl,J,ICase,Pmf,Plf).
-jouerIci(IMorp,Pm,Pl,J,ICase,Pmf,Plf):-
-	jouer(IMorp,Pm,Pl,J,ICase,Pmf,Plf).
+	append(BeforeIm0,[NV|_],Pm),!, % morpion IMorp0 terminé
+	selectionnerCaseJouable(Pm,IMorp).
+trouverMorpionJouable(_,IMorp,IMorp).
+
+%%%%%%
 
 verifierMorpionGagnant(Pm,Morp,_,Pm):-
 	\+morpionTermine(Morp),!.
@@ -101,14 +112,18 @@ morpionRempli([J|M]):-
 	joueur(J),
 	morpionRempli(M).
 
-deroulement(_,Pm,Pl,_,_,Pm,Pl):-
+deroulement(_,Pm,Pl,_,Pm,Pl):-
 	morpionTermine(Pm),!.
-deroulement(IMorp,Pm,Pl,J,_,Pmf,Plf):-
+deroulement(IMorp0,Pm,Pl,J,Pmf,Plf):-
+	trouverMorpionJouable(Pm,IMorp0,IMorp), % le coup se jouera dans le morpion IMorp
+	jouer(IMorp,Pm,Pl,J,ICase,Pm2,Pl2),
 	joueurSuivant(J,JS),
-	jouerIci(IMorp,Pm,Pl,J,ICase2,Pm2,Pl2),
-	deroulement(ICase2,Pm2,Pl2,JS,_,Pmf,Plf).
+	deroulement(ICase,Pm2,Pl2,JS,Pmf,Plf).
 
 test(Pmf,Plf):-
 	morpionVide(Pm),
 	plateauVide(Pl),
-	deroulement(-1,Pm,Pl,1,_,Pmf,Plf).
+	deroulement(-1,Pm,Pl,1,Pmf,Plf).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
