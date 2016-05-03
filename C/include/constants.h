47cd4dc10edc9b5f6b5ebb55a05c69cc04027728
jmaire <jmaire@users.noreply.github.com>
2016-05-03 13:29:59 +0200

    zg

--- C/include/constants.h
+++ C/include/constants.h
@@ -5,6 +5,9 @@
 
 static const char* OURNAME = "JulIA";
 
+static char* NOM_MACHINE = "localhost";
+static const int PORT_JAVA_SOCKET = 5555;
+
 static const clock_t TIMEOUT_DELAY = 6.0*CLOCKS_PER_SEC;
 
 #endif
