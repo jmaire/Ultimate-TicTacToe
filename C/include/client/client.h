03860a7b05f2efb17c30cde48957799a31623622
jmaire <jmaire@users.noreply.github.com>
2016-05-03 13:26:54 +0200

    zg

--- C/include/client/client.h
+++ C/include/client/client.h
@@ -6,13 +6,20 @@
 int demandePartie(int sock);
 int reponsePartie(int sock, TypPartieRep* initialisationData);
 
-int demandeCoup(int sock, TypCoupReq* coup, int tictactoeWon);
+int demandeCoup(int sock, int sockJava, TypCoupReq* coup);
 int reponseCoup(int sock);
 
-int aNousDeJouer(int sock, TypCoupReq* coup, int tictactoeWon);
+int aNousDeJouer(int sock, int sockJava, TypCoupReq* coup);
 
 int receptionCoupAdversaire(int sock, TypCoupReq* coup);
 
-int aToiDeJouer(int sock);
+int aToiDeJouer(int sock, int sockJava);
+
+/******************** JAVA ********************/
+
+int connexionJava(int* sock);
+
+int envoyerAJava(int sock, TypCoupReq* coup);
+int recevoirDeJava(int sock, TypCoupReq* coup);
 
 #endif //CLIENT_H
