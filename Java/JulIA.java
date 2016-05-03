185a904ce0120b6edb9fbfa628d1e9b2c73a3785
jmaire <jmaire@users.noreply.github.com>
2016-05-03 13:28:17 +0200

    zg

--- Java/JulIA.java
+++ Java/JulIA.java
@@ -5,30 +5,65 @@ import java.io.*;
 // pour utiliser les HashMap 
 import java.util.*;
 
+import java.net.Socket;
+import java.net.ServerSocket;
+import java.io.InputStream;
+import java.io.IOException;
+
 public class JulIA {
   private static final String PROLOG_FILE_PATH = "../Prolog/ai.pl";
   private static final String joueur = "1";
   private static final String CLE_COUP = "Coup";
+  private static final int PORT_SOCKET = 5555;
+  private static final int BYTE_TO_READ = 3*4;
 
-  private static int SAFE_AB_PROFONDEUR = 1;
-  private static int AB_PROFONDEUR = 5;
+  private static final int SAFE_AB_PROFONDEUR = 1;
+  private static int AB_PROFONDEUR = 1;
 
   private static int[][] plateau;
+  private static Socket sockComm = null;
 
   public static void main(String[] args) {
     plateau = new int[][] {
-                                                    new int[]{0,0,0,0,0,0,0,0,0},
-                                                    new int[]{0,0,0,0,0,0,0,0,0},
-                                                    new int[]{0,0,0,0,0,0,0,0,0},
-                                                    new int[]{0,0,0,0,0,0,0,0,0},
-                                                    new int[]{0,0,0,0,0,0,0,0,0},
-                                                    new int[]{0,0,0,0,0,0,0,0,0},
-                                                    new int[]{0,0,0,0,0,0,0,0,0},
-                                                    new int[]{0,0,0,0,0,0,0,0,0},
-                                                    new int[]{0,0,0,0,0,0,0,0,0},
-                                                   };
-    int[] coup = recupererCoup(plateau,-1);
-    System.out.println(":="+coup[0]+","+coup[1]);
+                  new int[]{0,0,0,0,0,0,0,0,0},
+                  new int[]{0,0,0,0,0,0,0,0,0},
+                  new int[]{0,0,0,0,0,0,0,0,0},
+                  new int[]{0,0,0,0,0,0,0,0,0},
+                  new int[]{0,0,0,0,0,0,0,0,0},
+                  new int[]{0,0,0,0,0,0,0,0,0},
+                  new int[]{0,0,0,0,0,0,0,0,0},
+                  new int[]{0,0,0,0,0,0,0,0,0},
+                  new int[]{0,0,0,0,0,0,0,0,0},
+                };
+
+    ServerSocket srv = null;
+
+    try {
+      srv = new ServerSocket(PORT_SOCKET);
+      sockComm = srv.accept();
+    
+      recevoirDeC();
+
+      sockComm.close();
+      srv.close(); // ferme la socket de comm et de connex
+    } catch(IOException e) {
+      //TODO
+      System.err.println("Exception io : " + e);
+    }
+    /*int[] coup = recupererCoup(plateau,-1);
+    System.out.println(":="+coup[0]+","+coup[1]);*/
+  }
+
+  public static void recevoirDeC() throws IOException {
+    InputStream is = sockComm.getInputStream();
+    byte[] tabloServ = new byte[BYTE_TO_READ];
+    if(BYTE_TO_READ!=is.read(tabloServ))
+        {}//TODO
+      for(int i=0;i<BYTE_TO_READ;i++){
+        System.out.print(tabloServ[i]);
+      }
+      System.out.println("");
+    is.close();
   }
 
   public static int[] recupererCoup(int[][] plateau, int imorpion) {
@@ -65,7 +100,6 @@ public class JulIA {
 
     //Coup normal
     saisie = "prochainCoup("+AB_PROFONDEUR+","+stPlateau+","+imorpion+","+joueur+","+CLE_COUP+").";
-    System.out.println(saisie);
     results = new HashMap();
     try {
       //TODO threadé timeout
